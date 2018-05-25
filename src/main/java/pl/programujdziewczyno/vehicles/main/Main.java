22bc0c20a0a0dbc803cc3aa765d22428fa43f4d4
Alicja <alicjacent@gmail.com>
2018-05-25 20:11:18 +0200

    initial commit

---
+++ src/main/java/pl/programujdziewczyno/vehicles/main/Main.java
@@ -0,0 +1,11 @@
+package pl.programujdziewczyno.vehicles.main;
+
+/**
+ * Created by Lalu on 25.05.2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("działam!");
+    }
+}
